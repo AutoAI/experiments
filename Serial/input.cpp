89cd48f7587fe3bd5605f5a44713c147608b9577
Parth Mehrotra <parth.mehrotra.cs@gmail.com>
2015-09-16 10:40:49 -0400

    added roach-term

--- Serial/input.cpp
+++ Serial/input.cpp
@@ -2,14 +2,15 @@
 
 int main() {
 	while (true) {
+		FILE *serPort = fopen("ff", "w");
 		unsigned int number;
-		
-		printf(">>>");
-		
+		printf(">>> ");
 		scanf("%x", &number);
-
-		printf("%d\n", number);
-
 		number = 0;
+
+		char writeBuffer[] = { number };
+		fwrite(writeBuffer, sizeof(char), sizeof(writeBuffer), serPort);
+		fclose(serPort);
+		return 0;
 	}
 }
